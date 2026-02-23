b01e74456f5642ec783dfba8baf9df7dd4d202f4
xDanElisia <32947233+Chronosaurus69@users.noreply.github.com>
2026-02-23 12:22:16 +0700

    Update superior_instantnoodle.mk

--- superior_instantnoodle.mk
+++ superior_instantnoodle.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodle device
 $(call inherit-product, device/oneplus/instantnoodle/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 PRODUCT_NAME := superior_instantnoodle
 PRODUCT_DEVICE := instantnoodle
 PRODUCT_MANUFACTURER := OnePlus
